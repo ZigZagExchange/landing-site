5b302345b04a209511e0f2762bf69c6008325951
WonderDev21 <wonder.dev21@gmail.com>
2022-10-31 21:16:11 +0900

    fix: server installation

---
+++ server/routes/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+function routes(app) {
+  router.get("/movies", (req, res) => {
+    res.end("We made it! And it's great");
+  });
+
+  router.get("/movies/:id", (req, res) => {
+    return app.render(req, res, "/movies", { id: req.params.id });
+  });
+
+  return router;
+}
+
+module.exports = routes;
